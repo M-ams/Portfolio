5802a545198437ee533f8118a796e23e2ec99e62
Mams-sy <93870019+Mams-sy@users.noreply.github.com>
2024-01-16 17:25:55 +0100

    first project

--- portfolio/pages/homepage.tsx
+++ portfolio/pages/homepage.tsx
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 function Homepage() {
   return (
@@ -15,30 +14,15 @@ function Homepage() {
           </div>
         </div>
         <div className="text-white text-9xl pt-[35vh] font gg flex flex-col font-light">
-          <Link href="/projects/projects" className="">
+          <Link href="/projects/projects" className="ff">
             PROJECTS
           </Link>
-          <a href="#" className="pt-10">
+          <Link href="#" className="pt-10 ff">
             ABOUT ME
-          </a>
-          <a href="#" className="pt-10">
+          </Link>
+          <Link href="#" className="pt-10 ff">
             CONTACT
-          </a>
-        </div>
-      </div>
-
-
-
-      <div id="projects" className="bg-[#121111] h-screen flex sm">
-        <div className=" text-white pl-40 pt-20 cssanimation sequence fadeInBottom dd">
-          <div className="flex text-2xl font-extralight">
-            <p className="mams">Hello, I am SY Mamadou,</p>
-            <p className="dev ml-1">Full Stack Web Developer</p>
-          </div>
-          <div className="flex text-2xl font-extralight">
-            <p>but you can call me</p>
-            <p className="ml-2 change-text">Mams</p>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
--- portfolio/pages/projects/projects.tsx
+++ portfolio/pages/projects/projects.tsx
@@ -1,9 +1,47 @@
+import Link from "next/link";
+
 function Projects() {
-    return (
-        <div>
-            Enter
+  return (
+    <div className="bg-[#121111] h-auto flex flex-col">
+      <div className="">
+        <p className="text-white text-6xl m-20 font-extralight cssanimation sequence fadeInBottom">
+          Some of my Works
+        </p>
+      </div>
+
+      <div className="flex flex-col gg">
+        <div className="divdcgg">
+          <Link
+            href="https://dc.gg"
+            className="text-white text-5xl ml-[10vw] sparkle u-hover--sparkle dcgg items-center justify-center flex w-[15Vw] h-[10vh]"
+            target="_blank"
+          >
+            DC.GG
+          </Link>
         </div>
-    );
+
+        <div className="flex h-[50vh]  ml-[17vw] m-5">
+          <div className="flex flex-col text-center text-2xl courier font-extralight">
+            <div className="dcggtext text-white  m-auto ">
+              <span className="dark-purple">DC.GG</span> is a group
+              end-of-studies project. During this project, I had the opportunity
+              to collaborate with a team of 4 individuals, focusing mainly on
+              the front-end aspect. The purpose of this project is to enable
+              Discord servers to generate their own personalized invitation
+              links with a functionnality of real-time tracking.
+            </div>
+            <span className="dark-purple mb-20">
+              TypeScript, NextJs, Prisma, ExpressJs
+            </span>
+          </div>
+          <img
+            src="/assets/images/dcgg.png"
+            className=" h-[38vh] ml-5 m-auto"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
