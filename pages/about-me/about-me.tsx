0ebdd05fa724f44e92e99cd32709e6e09f409349
Mams-sy <93870019+Mams-sy@users.noreply.github.com>
2024-01-17 15:39:27 +0100

    about me exp pro

--- pages/about-me/about-me.tsx
+++ pages/about-me/about-me.tsx
@@ -9,18 +9,16 @@ import { TbBrandMysql } from "react-icons/tb";
 import { FaNodeJs } from "react-icons/fa";
 import { TbBrandNextjs } from "react-icons/tb";
 import { SiExpress } from "react-icons/si";
-
-
-
+import Link from "next/link";
 
 function AboutMe() {
   return (
     <div className="bg-[#121111] h-auto flex flex-col">
       <div className="">
-        <p className="text-white text-6xl m-20 font-extralight cssanimation sequence fadeInBottom">
+        <p className="text-white text-6xl m-20 cssanimation sequence fadeInBottom font-bold">
           What about me ?
         </p>
-        <div className="flex ml-40">
+        <div className="flex ml-40 gg">
           <div className="flex flex-col text-gray-500 h-auto w-[40vw] text-left">
             <div className="text-2xl mt-4">
               Well, now you know my nickname but for me it&apos;s not enough, so
@@ -97,10 +95,51 @@ function AboutMe() {
           />
         </div>
 
-        <p className="text-white text-3xl m-20 font-extralight cssanimation sequence fadeInBottom">
-          Here is some skills that I have discovered and have been able to
-          practice
+        <p className="text-white text-6xl m-20 cssanimation sequence fadeInBottom font-bold">
+          Where did I have work for ?
         </p>
+        <div className="flex flex-col ml-40 ">
+          <p className="text-gray-500 text-3xl bb">
+            Full Stack Web Developer at{" "}
+            <Link
+              href="https://www.uptodo.fr"
+              target="_blank"
+              className="text-gray-200 effect-shine"
+            >
+              UPTODO
+            </Link>
+          </p>
+          <p className="text-gray-200 text-1xl ml-2">Sep 2022 - Nov 2023</p>
+        </div>
+        <div className="flex ml-60 gg">
+          <div className="flex flex-col text-gray-500 h-auto w-[40vw] text-left">
+            <div className="text-2xl mt-8">
+              At uptodo I participated in the development of the{" "}
+              <span className="text-gray-200">Actadat </span>
+              software. The languages were{" "}
+              <span className="text-gray-200">Symfony</span> and{" "}
+              <span className="text-gray-200">Javascript</span>
+            </div>
+            <div className="text-2xl mt-4">
+              My missions were to develop <span className="text-gray-200">new features</span> based on
+              customer feedback and update them. I also participated in the
+              meetings to reflect on the application evolution perspectives.
+            </div>
+            <div className="text-2xl mt-4">
+              This very <span className="text-gray-200">enriching</span> first experience allowed me to
+              develop my skills, see how <span className="text-gray-200">software development</span> works
+              with production etc ... and prepare myself for the professional
+              world.
+            </div>
+          </div>
+          <Image
+            src=""
+            alt="test"
+            width={400}
+            height={400}
+            className="border rounded-3xl m-auto"
+          />
+        </div>
       </div>
     </div>
   );
