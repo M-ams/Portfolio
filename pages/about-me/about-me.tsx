1f03a976dddaca082f2f5165700ca99ba35b6e8c
Mams-sy <93870019+Mams-sy@users.noreply.github.com>
2024-01-17 19:21:12 +0100

    about me add button, fix animation homepage

--- pages/about-me/about-me.tsx
+++ pages/about-me/about-me.tsx
@@ -9,137 +9,151 @@ import { TbBrandMysql } from "react-icons/tb";
 import { FaNodeJs } from "react-icons/fa";
 import { TbBrandNextjs } from "react-icons/tb";
 import { SiExpress } from "react-icons/si";
+
 import Link from "next/link";
+
+
+
+
 
 function AboutMe() {
   return (
     <div className="bg-[#121111] h-auto flex flex-col">
-      <div className="">
-        <p className="text-white text-6xl m-20 cssanimation sequence fadeInBottom font-bold">
-          What about me ?
-        </p>
-        <div className="flex ml-40 gg">
-          <div className="flex flex-col text-gray-500 h-auto w-[40vw] text-left">
-            <div className="text-2xl mt-4">
-              Well, now you know my nickname but for me it&apos;s not enough, so
-              let&apos;s go.
+      <p className="text-white text-6xl m-20 cssanimation sequence fadeInBottom font-bold">
+        What about me ?
+      </p>
+      <div className="flex ml-40 gg">
+        <div className="flex flex-col text-gray-500 h-auto w-[40vw] text-left">
+          <div className="text-2xl mt-4">
+            Well, now you know my nickname but for me it&apos;s not enough, so
+            let&apos;s go.
+          </div>
+          <div className="text-2xl mt-8">
+            Since childhood I have always had an attraction towards computers.
+            After some adventures, back in{" "}
+            <span className="text-gray-200">2021</span> where I found myself
+            doing a<span className="text-gray-200"> web development</span>{" "}
+            school for a period of 2 years. During which I was able to discover
+            the world of work in this field.
+          </div>
+          <div className="text-2xl mt-8 mb-4">
+            My main goal these days is to{" "}
+            <span className="text-gray-200">improve</span> my web skills by
+            integrating a company with a{" "}
+            <span className="text-gray-200">passionate</span> team that will
+            make me a better
+            <span className="text-gray-200"> developer</span>
+          </div>
+          <div className="text-2xl mt-8 mb-4">
+            Here is some languages that I have discovered and have been able to
+            practice
+          </div>
+          <div className="flex mt-6">
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float1">
+              <SiPhp className="text-5xl m-auto" />
+              <div className="m-auto">PHP</div>
             </div>
-            <div className="text-2xl mt-8">
-              Since childhood I have always had an attraction towards computers.
-              After some adventures, back in{" "}
-              <span className="text-gray-200">2021</span> where I found myself
-              doing a<span className="text-gray-200"> web development</span>{" "}
-              school for a period of 2 years. During which I was able to
-              discover the world of work in this field.
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float4">
+              <FaSymfony className="text-5xl m-auto" />
+              <div className="m-auto">Symfony</div>
             </div>
-            <div className="text-2xl mt-8 mb-4">
-              My main goal these days is to{" "}
-              <span className="text-gray-200">improve</span> my web skills by
-              integrating a company with a{" "}
-              <span className="text-gray-200">passionate</span> team that will
-              make me a better
-              <span className="text-gray-200"> developer</span>
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float5">
+              <IoLogoJavascript className="text-5xl m-auto" />
+              <div className="m-auto">Javascript</div>
             </div>
-            <div className="text-2xl mt-8 mb-4">
-              Here is some languages that I have discovered and have been able
-              to practice
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float3">
+              <FaReact className="text-5xl m-auto" />
+              <div className="m-auto">ReactJs</div>
             </div>
-            <div className="flex mt-6">
-              <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float1">
-                <SiPhp className="text-5xl m-auto" />
-                <div className="m-auto">PHP</div>
-              </div>
-              <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float4">
-                <FaSymfony className="text-5xl m-auto" />
-                <div className="m-auto">Symfony</div>
-              </div>
-              <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float5">
-                <IoLogoJavascript className="text-5xl m-auto" />
-                <div className="m-auto">Javascript</div>
-              </div>
-              <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float3">
-                <FaReact className="text-5xl m-auto" />
-                <div className="m-auto">ReactJs</div>
-              </div>
-              <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float2">
-                <SiTypescript className="text-5xl m-auto" />
-                <div className="m-auto">Typescript</div>
-              </div>
-            </div>
-
-            <div className="flex mt-10">
-              <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float1">
-                <TbBrandMysql className="text-5xl m-auto" />
-                <div className="m-auto">MySQL</div>
-              </div>
-              <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float4">
-                <FaNodeJs className="text-5xl m-auto" />
-                <div className="m-auto">NodeJs</div>
-              </div>
-              <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float2">
-                <TbBrandNextjs className="text-5xl m-auto" />
-                <div className="m-auto">NextJs</div>
-              </div>
-              <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float5">
-                <SiExpress className="text-5xl m-auto" />
-                <div className="m-auto">ExpressJs</div>
-              </div>
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float2">
+              <SiTypescript className="text-5xl m-auto" />
+              <div className="m-auto">Typescript</div>
             </div>
           </div>
-          <Image
-            src=""
-            alt="test"
-            width={400}
-            height={400}
-            className="border rounded-3xl m-auto"
-          />
-        </div>
 
-        <p className="text-white text-6xl m-20 cssanimation sequence fadeInBottom font-bold">
-          Where did I have work for ?
-        </p>
-        <div className="flex flex-col ml-40 ">
-          <p className="text-gray-500 text-3xl bb">
-            Full Stack Web Developer at{" "}
-            <Link
-              href="https://www.uptodo.fr"
-              target="_blank"
-              className="text-gray-200 effect-shine"
-            >
-              UPTODO
-            </Link>
-          </p>
-          <p className="text-gray-200 text-1xl ml-2">Sep 2022 - Nov 2023</p>
-        </div>
-        <div className="flex ml-60 gg">
-          <div className="flex flex-col text-gray-500 h-auto w-[40vw] text-left">
-            <div className="text-2xl mt-8">
-              At uptodo I participated in the development of the{" "}
-              <span className="text-gray-200">Actadat </span>
-              software. The languages were{" "}
-              <span className="text-gray-200">Symfony</span> and{" "}
-              <span className="text-gray-200">Javascript</span>
+          <div className="flex mt-10">
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float1">
+              <TbBrandMysql className="text-5xl m-auto" />
+              <div className="m-auto">MySQL</div>
             </div>
-            <div className="text-2xl mt-4">
-              My missions were to develop <span className="text-gray-200">new features</span> based on
-              customer feedback and update them. I also participated in the
-              meetings to reflect on the application evolution perspectives.
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float4">
+              <FaNodeJs className="text-5xl m-auto" />
+              <div className="m-auto">NodeJs</div>
             </div>
-            <div className="text-2xl mt-4">
-              This very <span className="text-gray-200">enriching</span> first experience allowed me to
-              develop my skills, see how <span className="text-gray-200">software development</span> works
-              with production etc ... and prepare myself for the professional
-              world.
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float2">
+              <TbBrandNextjs className="text-5xl m-auto" />
+              <div className="m-auto">NextJs</div>
+            </div>
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float5">
+              <SiExpress className="text-5xl m-auto" />
+              <div className="m-auto">ExpressJs</div>
             </div>
           </div>
-          <Image
-            src=""
-            alt="test"
-            width={400}
-            height={400}
-            className="border rounded-3xl m-auto"
-          />
         </div>
+        <Image
+          src=""
+          alt="test"
+          width={400}
+          height={400}
+          className="border rounded-3xl m-auto"
+        />
+      </div>
+
+      <p className="text-white text-6xl m-20 cssanimation sequence fadeInBottom font-bold">
+        Where I&apos;ve worked ?
+      </p>
+      <div className="flex flex-col ml-40 gg">
+        <p className="text-gray-500 text-3xl bb">
+          Full Stack Web Developer at{" "}
+          <Link
+            href="https://www.uptodo.fr"
+            target="_blank"
+            className="text-gray-200 effect-shine"
+          >
+            UPTODO
+          </Link>
+        </p>
+        <p className="text-gray-200 text-1xl ml-2">Sep 2022 - Nov 2023</p>
+      </div>
+      <div className="flex ml-60 gg">
+        <div className="flex flex-col text-gray-500 h-auto w-[40vw] text-left">
+          <div className="text-2xl mt-8">
+            At uptodo I participated in the development of the{" "}
+            <span className="text-gray-200">Actadat </span>
+            software. The languages were{" "}
+            <span className="text-gray-200">Symfony</span> and{" "}
+            <span className="text-gray-200">Javascript</span>
+          </div>
+          <div className="text-2xl mt-4">
+            My missions were to develop{" "}
+            <span className="text-gray-200">new features</span> based on
+            customer feedback and update them. I also participated in the
+            meetings to reflect on the application evolution perspectives.
+          </div>
+          <div className="text-2xl mt-4">
+            This very <span className="text-gray-200">enriching</span> first
+            experience allowed me to develop my skills, see how{" "}
+            <span className="text-gray-200">software development</span> works
+            with production etc ... and prepare myself for the professional
+            world.
+          </div>
+        </div>
+        <Image
+          src=""
+          alt="test"
+          width={400}
+          height={400}
+          className="border rounded-3xl m-auto"
+        />
+      </div>
+      <div className="flex m-auto mt-10 space-x-6 cssanimation sequence fadeInBottom">
+        <Link href="/" className="btn btn--stripe btn--radius">
+          Home
+        </Link>
+
+        <a href="/assets/cv/cv.pdf" target="_blank" className="btn btn--stripe btn--radius">
+          Resume
+        </a>
       </div>
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,16 @@ import { useState, useEffect } from "react";
 import Image from "next/image";
 import Mams from "./Mams";
 import Homepage from "./homepage";
+import Cookies from "js-cookie";
 
 export default function Home() {
-  const [showMams, setShowMams] = useState(false);
+  const [showMams, setShowMams] =  useState<boolean>(!!Cookies.get('animation'));
+  /* check if cookie existe dans la fonctionn usestate */
   useEffect(() => {
     setTimeout(() => {
       setShowMams(true);
+      /* ajouter cookie */ 
+      Cookies.set("animation", "true",{ expires: 1 / 288, path: '' });
     }, 3000);
   }, []);
 
