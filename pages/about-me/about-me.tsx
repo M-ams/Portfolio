813d9eae38b40b820c80e5078355832175d76e85
Mams-sy <93870019+Mams-sy@users.noreply.github.com>
2024-01-18 23:32:31 +0100

    about responsive

--- pages/about-me/about-me.tsx
+++ pages/about-me/about-me.tsx
@@ -12,23 +12,19 @@ import { SiExpress } from "react-icons/si";
 
 import Link from "next/link";
 
-
-
-
-
 function AboutMe() {
   return (
-    <div className="bg-[#121111] h-[100vh] flex flex-col">
-      <p className="text-white text-6xl m-20 cssanimation sequence fadeInBottom font-bold">
+    <div className="bg-[#121111] min-h-screen flex flex-col">
+      <p className="text-white text-[3vw] xs:text-[5vw] xs:text-center md:text-center m-20 font-bold cssanimation sequence fadeInBottom">
         What about me ?
       </p>
-      <div className="flex ml-40 gg">
-        <div className="flex flex-col text-gray-500 h-auto w-[40vw] text-left">
-          <div className="text-2xl mt-4">
+      <div className="xl:flex 2xl:flex lg:flex xl:ml-40 2xl:ml-40 lg:ml-40 xs:m-auto md:m-auto gg">
+        <div className="flex flex-col text-gray-500 h-auto xs:w-[80vw] md:w-[80vw]  2xl:w-[60vw] xl:w-[40vw] lg:w-[40vw] xl:text-left 2xl:text-left lg:text-left xs:text-center md:text-center ">
+          <div className="text-[2vw] 2xl:text-[3vw] xs:text-[5vw] md:text-[3vw] mt-4">
             Well, now you know my nickname but for me it&apos;s not enough, so
             let&apos;s go.
           </div>
-          <div className="text-2xl mt-8">
+          <div className="text-[2vw] xs:text-[5vw] md:text-[3vw] mt-8">
             Since childhood I have always had an attraction towards computers.
             After some adventures, back in{" "}
             <span className="text-gray-200">2021</span> where I found myself
@@ -36,7 +32,7 @@ function AboutMe() {
             school for a period of 2 years. During which I was able to discover
             the world of work in this field.
           </div>
-          <div className="text-2xl mt-8 mb-4">
+          <div className="text-[2vw]  xs:text-[5vw] md:text-[3vw] mt-8 mb-4">
             My main goal these days is to{" "}
             <span className="text-gray-200">improve</span> my web skills by
             integrating a company with a{" "}
@@ -44,48 +40,48 @@ function AboutMe() {
             make me a better
             <span className="text-gray-200"> developer</span>
           </div>
-          <div className="text-2xl mt-8 mb-4">
+          <div className="text-[2vw] xs:text-[5vw] md:text-[3vw] mt-8 mb-4">
             Here is some languages that I have discovered and have been able to
             practice
           </div>
-          <div className="flex mt-6">
-            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float1">
-              <SiPhp className="text-5xl m-auto" />
+          <div className="xl:flex lg:flex 2xl:flex mt-6">
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float1 xs:w-full md:w-full">
+              <SiPhp className="text-[4vw] xs:text-[12vw] md:text-[8vw] m-auto" />
               <div className="m-auto">PHP</div>
             </div>
-            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float4">
-              <FaSymfony className="text-5xl m-auto" />
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float4 xs:w-full md:w-full">
+              <FaSymfony className="text-[4vw] xs:text-[12vw] md:text-[8vw] m-auto" />
               <div className="m-auto">Symfony</div>
             </div>
-            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float5">
-              <IoLogoJavascript className="text-5xl m-auto" />
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float5 xs:w-full md:w-full">
+              <IoLogoJavascript className="text-[4vw] xs:text-[12vw] md:text-[8vw] m-auto" />
               <div className="m-auto">Javascript</div>
             </div>
-            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float3">
-              <FaReact className="text-5xl m-auto" />
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float3 xs:w-full md:w-full">
+              <FaReact className="text-[4vw] xs:text-[12vw] md:text-[8vw] m-auto" />
               <div className="m-auto">ReactJs</div>
             </div>
-            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float2">
-              <SiTypescript className="text-5xl m-auto" />
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float2 xs:w-full md:w-full">
+              <SiTypescript className="text-[4vw] xs:text-[12vw] md:text-[8vw] m-auto" />
               <div className="m-auto">Typescript</div>
             </div>
           </div>
 
-          <div className="flex mt-10">
-            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float1">
-              <TbBrandMysql className="text-5xl m-auto" />
+          <div className="xl:flex lg:flex 2xl:flex xl:mt-10 2xl:mt-10 lg:mt-10 ">
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float1 xs:w-full md:w-full">
+              <TbBrandMysql className="text-[4vw] xs:text-[12vw] md:text-[8vw] m-auto" />
               <div className="m-auto">MySQL</div>
             </div>
-            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float4">
-              <FaNodeJs className="text-5xl m-auto" />
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float4 xs:w-full md:w-full">
+              <FaNodeJs className="text-[4vw] xs:text-[12vw] md:text-[8vw] m-auto" />
               <div className="m-auto">NodeJs</div>
             </div>
-            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float2">
-              <TbBrandNextjs className="text-5xl m-auto" />
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float2 xs:w-full md:w-full">
+              <TbBrandNextjs className="text-[4vw] xs:text-[12vw] md:text-[8vw] m-auto" />
               <div className="m-auto">NextJs</div>
             </div>
-            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float5">
-              <SiExpress className="text-5xl m-auto" />
+            <div className="text-gray-200 flex flex-col m-auto  w-[6vw] float5 xs:w-full md:w-full">
+              <SiExpress className="text-[4vw] xs:text-[12vw] md:text-[8vw] m-auto" />
               <div className="m-auto">ExpressJs</div>
             </div>
           </div>
@@ -95,15 +91,15 @@ function AboutMe() {
           alt="test"
           width={400}
           height={200}
-          className="rounded-3xl m-auto"
+          className="rounded-3xl m-auto xs:w-[40vw] md:w-[40vw] "
         />
       </div>
 
-      <p className="text-white text-6xl m-20 cssanimation sequence fadeInBottom font-bold">
+      <p className="text-white text-[3vw] xs:text-[5vw] md:text-center m-20 font-bold cssanimation sequence fadeInBottom">
         Where I&apos;ve worked ?
       </p>
-      <div className="flex flex-col ml-40 gg">
-        <p className="text-gray-500 text-3xl bb">
+      <div className="xl:flex flex-col 2xl:flex lg:flex xl:ml-40 2xl:ml-40 lg:ml-40 xs:m-auto md:m-auto xs:text-center md:text-center  gg">
+        <p className="text-gray-500 text-[2vw] xs:text-[5vw] md:text-[5vw] bb">
           Full Stack Web Developer at{" "}
           <Link
             href="https://www.uptodo.fr"
@@ -113,24 +109,26 @@ function AboutMe() {
             UPTODO
           </Link>
         </p>
-        <p className="text-gray-200 text-1xl ml-2">Sep 2022 - Nov 2023</p>
+        <p className="text-gray-200 xl:text-[2vw] 2xl:text-[2vw] lg:text-[2vw] 2xl:ml-[1vw] xl:ml-[1vw] lg:ml-[1vw]">
+          Sep 2022 - Nov 2023
+        </p>
       </div>
-      <div className="flex ml-60 gg">
-        <div className="flex flex-col text-gray-500 h-auto w-[40vw] text-left">
-          <div className="text-2xl mt-8">
+      <div className="xl:flex 2xl:flex lg:flex xl:ml-60 2xl:ml-60 lg:ml-60 xs:m-auto md:m-auto gg">
+        <div className="flex flex-col text-gray-500 h-auto xs:w-[80vw] md:w-[80vw]  2xl:w-[40vw] xl:w-[40vw] lg:w-[40vw] xl:text-left 2xl:text-left lg:text-left xs:text-center md:text-center">
+          <div className="text-[2vw] xs:text-[5vw] mt-8">
             At uptodo I participated in the development of the{" "}
             <span className="text-gray-200">Actadat </span>
             software. The languages were{" "}
             <span className="text-gray-200">Symfony</span> and{" "}
             <span className="text-gray-200">Javascript</span>
           </div>
-          <div className="text-2xl mt-4">
+          <div className="text-[2vw] xs:text-[5vw] mt-4">
             My missions were to develop{" "}
             <span className="text-gray-200">new features</span> based on
             customer feedback and update them. I also participated in the
             meetings to reflect on the application evolution perspectives.
           </div>
-          <div className="text-2xl mt-4">
+          <div className="text-[2vw] xs:text-[5vw] mt-4">
             This very <span className="text-gray-200">enriching</span> first
             experience allowed me to develop my skills, see how{" "}
             <span className="text-gray-200">software development</span> works
@@ -140,11 +138,15 @@ function AboutMe() {
         </div>
       </div>
       <div className="flex m-auto mt-10 space-x-6 cssanimation sequence fadeInBottom">
-        <Link href="/" className="btn btn--stripe btn--radius">
+        <Link href="/" className="btn btn--stripe btn--radius 2xl:text-[2vw]">
           Home
         </Link>
 
-        <a href="/assets/cv/cv.pdf" target="_blank" className="btn btn--stripe btn--radius">
+        <a
+          href="/assets/cv/cv.pdf"
+          target="_blank"
+          className="btn btn--stripe btn--radius 2xl:text-[2vw]"
+        >
           Resume
         </a>
       </div>
