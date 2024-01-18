29e92345d856070fd70205e1f592ee375b90ba26
Mams-sy <93870019+Mams-sy@users.noreply.github.com>
2024-01-18 19:32:12 +0100

    contact page responsive

--- pages/contact/contact.tsx
+++ pages/contact/contact.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 function Contact() {
   return (
     <div className="bg-[#121111] min-h-screen flex flex-col">
-      <div className="flex flex-col mt-[25vh] ml-[17vw] optima">
-        <p className="text-9xl text-white gg">Hello.</p>
-        <p className="text-2xl text-white mt-10 font-thin gg2">
+      <div className="flex flex-col xl:mt-[25vh] xl:ml-[17vw] lg:mt-[25vh] lg:ml-[17vw] 2xl:mt-[25vh] 2xl:ml-[17vw] optima xs:text-center sm:text-center md:text-center ">
+        <p className="text-[10vw] xs:text-6xl lg:text-6xl xs:mt-32 lg:mt-32 md:mt-32 text-white gg">Hello.</p>
+        <p className="text-[2vw] xs:text-[5vw] sm:text-[5vw] md:text-[5vw] text-white mt-10 font-thin gg2">
           Interested in my profile ? Get in touch
         </p>
-        <p className="text-2xl text-white mt-6 font-thin gg2">
+        <p className="text-[2vw] xs:text-[5vw] text-white mt-6 font-thin gg2">
           Email:{" "}
           <Link href="mailto:mamadou.sy143@gmail.com" className="underline">
             {" "}
             mamadou.sy143@gmail.com
           </Link>
         </p>
-        <p className="text-2xl text-white mt-6 font-thin gg3">
+        <p className="text-[2vw] xs:text-[5vw] text-white mt-6 font-thin gg3">
           On the internet:{" "}
           <Link href="https://www.linkedin.com/in/mamadou-sy/" className="underline">
             {" "}
@@ -27,8 +27,8 @@ function Contact() {
             </Link>
         </p>
       </div>
-      <div className="flex m-auto mt-[25vh] space-x-6 cssanimation sequence fadeInBottom">
-        <Link href="/" className="btn btn--stripe btn--radius">
+      <div className="flex m-auto mt-[15vh] space-x-6 cssanimation sequence fadeInBottom ">
+        <Link href="/" className="btn btn--stripe btn--radius 2xl:text-[4vw]">
           Home
         </Link>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,17 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: {
+        'xs': {'min': '300px', 'max': '639px'},  // Extra small screens
+        'sm': {'min': '640px', 'max': '767px'},  // Small screens
+        'md': {'min': '768px', 'max': '1023px'}, // Medium screens
+        'lg': {'min': '1024px', 'max': '1279px'}, // Large screens
+        'xl': {'min': '1280px', 'max': '1535px'}, // Extra large screens
+        '2xl': {'min': '1536px'}, // 2x large screens (no max-width)
+      },
     },
   },
   plugins: [],
 }
+
 export default config
