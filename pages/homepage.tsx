f9b518e8430f89eeb5364d26ae9d1596dd3739b7
Mams-sy <93870019+Mams-sy@users.noreply.github.com>
2024-01-20 16:36:13 +0100

    link social media

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -2,24 +2,50 @@ import Link from "next/link";
 
 import { FaFacebook, FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 import Navbar from "../components/Navbar";
-import { Box, Button, Container, Flex, HStack, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 function Homepage() {
   return (
     <Box className=" min-h-screen flex" bg={"brand.bg"}>
       <Navbar />
 
-      <Stack mt={["20vh", "20vh", "35vh", "35vh"]} w={'100%'} marginLeft={'5vh'}>
-        <Text fontSize={{ base: "50px", md: "80px", lg: "80px", xl:'80px' }} color={'brand.text2'}>Hey, I&apos;m Mamadou Sy</Text>
-        <Text fontSize={{ base: "40px", md: "40px", lg: "40px", xl:'40px' }} color={'brand.text2'}>Full Stack Developer, But you can call me Mams</Text>
+      <Stack
+        mt={["20vh", "20vh", "35vh", "35vh"]}
+        w={"100%"}
+        marginLeft={"5vh"}
+      >
+        <Text
+          fontSize={{ base: "50px", md: "80px", lg: "80px", xl: "80px" }}
+          color={"brand.text2"}
+        >
+          Hey, I&apos;m Mamadou Sy
+        </Text>
+        <Text
+          fontSize={{ base: "40px", md: "40px", lg: "40px", xl: "40px" }}
+          color={"brand.text2"}
+        >
+          Full Stack Developer, But you can call me Mams
+        </Text>
         <HStack>
-  <Button bg="black" color={'white'} leftIcon={<FaGithub />}>
-    Github
-  </Button>
-  <Button colorScheme='linkedin' leftIcon={<FaLinkedin />}>
-    Linkedin
-  </Button>
-</HStack>
+          <Link href={"https://github.com/Mams-sy"} target="_blank">
+            <Button bg="black" color={"white"} leftIcon={<FaGithub />}>
+              Github
+            </Button>
+          </Link>
+          <Link href={"https://www.linkedin.com/in/mamadou-sy/"} target="_blank">
+            <Button colorScheme="linkedin" leftIcon={<FaLinkedin />}>
+              Linkedin
+            </Button>
+          </Link>
+        </HStack>
       </Stack>
     </Box>
   );
