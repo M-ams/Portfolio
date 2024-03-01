81e496ca3d5c7e073a57dfa9d0bf8f006c9d0260
Mams-sy <93870019+Mams-sy@users.noreply.github.com>
2024-03-01 16:29:53 +0100

    fix bg

--- pages/Mams.tsx
+++ pages/Mams.tsx
@@ -1,6 +1,6 @@
 export default function Mams() {
     return (
-       <div className='flex p-4 items-center justify-center h-screen bg-[#121111] optima'>
+       <div className='flex p-4 items-center justify-center h-screen bg-[#111113] optima'>
       <p className='text-white m-0 text-[2vw] xs:text-[8vw] md:text-[6vw] 2xl:text-[5vw] font-light fade-out1'>SY Mamadou</p>
       <p className='text-white m-0 ml-3 text-[2vw] xs:text-[8vw] md:text-[6vw] 2xl:text-[5vw] font-thin fade-out2'>Portfolio</p>
     </div>
